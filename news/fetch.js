46d5d7ae38cff125262355dc10d5a481ef37e975
Mimrpim <mimrpim@gmail.com>
2026-09-18 17:11:42 +0200

    added news

---
+++ news/fetch.js
@@ -0,0 +1,14 @@
+async function getData() {
+  const url = "https://mimrpimstudios.github.io/news/fetch.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
